51c632ed3d92d6c16ea4354014342433685cbac2
joaopbueno <mrjoao16@gmail.com>
2024-12-05 13:19:22 -0300

    Inicial

---
+++ BancoApp/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  BancoApp
+//
+//  Created by João Bueno on 05/12/24.
+//
+import SwiftUI
+
+extension Color {
+    static var darkColor = Color("Dark")
+    static var whiteColor = Color("WhiteColor")
+    static var backgroundXColor = Color("BackgroundX")
+    static var fontColor = Color("FontColor")
+}
--- BancoApp/ContentView.swift
+++ BancoApp/ContentView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.darkColor
+                .ignoresSafeArea()
+            VStack(alignment: .leading) {
+                HStack {
+                    Spacer()
+                    Text("X")
+                        .font(.title)
+                        .foregroundColor(Color.whiteColor)
+                        .padding()
+                        .background(Color.backgroundXColor)
+                        .cornerRadius(10)
+                }
+                Image(systemName: "person.crop.circle")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(Color.fontColor)
+                    .background(Color.backgroundX)
+                    .clipShape(Circle())
+                    .padding()
+                
+                TextView(text: "João Bueno", font: .title)
+                TextView(text: "UX UI Designer", font: .headline)
+                Spacer()
+            }
         }
-        .padding()
+    }
+}
+
+struct TextView: View {
+    let text: String
+    let font: Font
+    
+    var body: some View {
+        Text(text)
+            .foregroundColor(Color.fontColor)
+            .font(font)
     }
 }
 
