26792f8a3a57acecafb1e628e5629b695fa8c5ce
joaopbueno <mrjoao16@gmail.com>
2024-12-05 10:46:49 -0300

    Initial Commit

---
+++ BancoApp/BancoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BancoAppApp.swift
+//  BancoApp
+//
+//  Created by João Bueno on 05/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BancoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
