eb6243d9ae9e4360d2dc04a72596fbe75c0ad0dd
joaopbueno <mrjoao16@gmail.com>
2024-12-09 08:53:38 -0300

    Atualização

--- BancoApp/ContentView.swift
+++ BancoApp/ContentView.swift
@@ -13,26 +13,50 @@ struct ContentView: View {
             Color.darkColor
                 .ignoresSafeArea()
             VStack(alignment: .leading) {
-                HStack {
-                    Spacer()
-                    Text("X")
-                        .font(.title)
-                        .foregroundColor(Color.whiteColor)
-                        .padding()
-                        .background(Color.backgroundXColor)
-                        .cornerRadius(10)
-                }
-                Image(systemName: "person.crop.circle")
-                    .resizable()
-                    .frame(width: 50, height: 50)
-                    .foregroundColor(Color.fontColor)
-                    .background(Color.backgroundX)
-                    .clipShape(Circle())
-                    .padding()
                 
-                TextView(text: "João Bueno", font: .title)
-                TextView(text: "UX UI Designer", font: .headline)
+                xView()
+                
+                AvatarView()
+                
+                ChoicesView()
+                
                 Spacer()
+            }
+        }
+    }
+}
+
+struct SingleChoiceView: View {
+    let text: String
+    let systemImage: String
+    var body: some View {
+        Label(text, systemImage: systemImage)
+            .font(.headline)
+            .foregroundStyle(Color.fontColor)
+            .padding()
+            .labelStyle(SpacedLabelStyle())
+    }
+}
+
+struct SpacedLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack(alignment: .center, spacing: 20){
+            configuration.icon
+            configuration.title
+        }
+    }
+}
+
+struct ChoicesView: View {
+    let dataArray: [(String, String)] = [(
+        "Corporate App",
+        "building.2"
+    )]
+    var body: some View {
+        VStack(alignment: .leading){
+            ForEach(Array(dataArray.enumerated()), id: \.0) { index, item in
+                
+                SingleChoiceView(text: item.0, systemImage: item.1)
             }
         }
     }
@@ -46,6 +70,38 @@ struct TextView: View {
         Text(text)
             .foregroundColor(Color.fontColor)
             .font(font)
+    }
+}
+
+struct AvatarView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image(systemName: "person.crop.circle")
+                .resizable()
+                .frame(width: 50, height: 50)
+                .foregroundColor(Color.fontColor)
+                .background(Color.backgroundX)
+                .clipShape(Circle())
+                .padding()
+            
+            TextView(text: "João Bueno", font: .largeTitle)
+            TextView(text: "UX UI Designer", font: .headline)
+        }
+        .padding()
+    }
+}
+
+struct xView: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            Text("X")
+                .font(.title)
+                .foregroundColor(Color.whiteColor)
+                .padding()
+                .background(Color.backgroundXColor)
+                .cornerRadius(10)
+        }
     }
 }
 
